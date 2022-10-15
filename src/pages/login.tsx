a2570661dee0225661876fab3dadc1e4c5536a54
Toxyk <toxyflog.games@gmail.com>
2022-10-15 19:27:26 +0300

    Login & Signup

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,19 +1,26 @@
-import React from "react";
-import {NextPage} from "next";
-import Form, {FormOnSubmitCallback} from "../components/login/Form";
+import React from 'react';
+import { NextPage } from 'next';
+import Form, { FormOnSubmitCallback } from '../components/login/Form';
 
 const Login: NextPage = () => {
-	const onSubmit: FormOnSubmitCallback = (inputs, showError) => {
-		if (inputs["username"].length < 4) return showError("Username can't be shorter than 4!");
-		if (inputs["username"].length > 30) return showError("Username can't be longer than 30!");
-		if (inputs["password"].length < 4) return showError("Password can't be shorter than 4!");
-		if (inputs["password"].length > 30) return showError("Password can't be longer than 30!");
+    const onSubmit: FormOnSubmitCallback = (inputs, showError) => {
+        if (inputs['username'].length < 4) return showError("Username can't be shorter than 4!");
+        if (inputs['username'].length > 30) return showError("Username can't be longer than 30!");
+        if (inputs['password'].length < 4) return showError("Password can't be shorter than 4!");
+        if (inputs['password'].length > 30) return showError("Password can't be longer than 30!");
 
-		// TODO
-	};
+        // TODO
+    };
 
-	return <Form title="Log in" subtitle="Don't have an account yet?" link={{text: "Sign up", href: "/signup"}} onSubmit={onSubmit}
-				 inputs={[{text: "username"}, {text: "password", isSecret: true}]}/>;
+    return (
+        <Form
+            title="Log in"
+            subtitle="Don't have an account yet?"
+            link={{ text: 'Sign up', href: '/signup' }}
+            onSubmit={onSubmit}
+            inputs={[{ text: 'username' }, { text: 'password', isSecret: true }]}
+        />
+    );
 };
 
 export default Login;
--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,20 +1,27 @@
-import React from "react";
-import {NextPage} from "next";
-import Form, {FormOnSubmitCallback} from "../components/login/Form";
+import React from 'react';
+import { NextPage } from 'next';
+import Form, { FormOnSubmitCallback } from '../components/login/Form';
 
 const Signup: NextPage = () => {
-	const onSubmit: FormOnSubmitCallback = (inputs, showError) => {
-		if (inputs["username"].length < 4) return showError("Username can't be shorter than 4!");
-		if (inputs["username"].length > 30) return showError("Username can't be longer than 30!");
-		if (inputs["password"].length < 4) return showError("Password can't be shorter than 4!");
-		if (inputs["password"].length > 30) return showError("Password can't be longer than 30!");
-		if (inputs["confirm"] !== inputs["password"]) return showError("Passwords don't match!");
+    const onSubmit: FormOnSubmitCallback = (inputs, showError) => {
+        if (inputs['username'].length < 4) return showError("Username can't be shorter than 4!");
+        if (inputs['username'].length > 30) return showError("Username can't be longer than 30!");
+        if (inputs['password'].length < 4) return showError("Password can't be shorter than 4!");
+        if (inputs['password'].length > 30) return showError("Password can't be longer than 30!");
+        if (inputs['confirm'] !== inputs['password']) return showError("Passwords don't match!");
 
-		// TODO
-	};
+        // TODO
+    };
 
-	return <Form title="Sign up" subtitle="Already have an account?" link={{text: "Log in", href: "/login"}} onSubmit={onSubmit}
-				 inputs={[{text: "username"}, {text: "password", isSecret: true}, {text: "Confirm password", key: "confirm", isSecret: true}]}/>;
+    return (
+        <Form
+            title="Sign up"
+            subtitle="Already have an account?"
+            link={{ text: 'Log in', href: '/login' }}
+            onSubmit={onSubmit}
+            inputs={[{ text: 'username' }, { text: 'password', isSecret: true }, { text: 'Confirm password', key: 'confirm', isSecret: true }]}
+        />
+    );
 };
 
 export default Signup;
