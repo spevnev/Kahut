569fe88e29c84947e9320c5e1904a3d1f4488544
Toxyk <toxyflog.games@gmail.com>
2022-10-28 16:04:47 +0300

    fix to subscriptions

--- src/pages/quiz/[code].tsx
+++ src/pages/quiz/[code].tsx
@@ -1,22 +1,18 @@
 import { gql, useSubscription } from '@apollo/client';
 import { GetServerSideProps, NextPage } from 'next';
-import { useRouter } from 'next/router';
 import { getCookie } from '../../utils/cookies';
 
 const GAME_SUBSCRIPTION = gql`
-    subscription gameSubscription($code: String!, $token: String!) {
-        onGameEvent(code: $code, token: $token)
+    subscription gameSubscription($token: String!) {
+        onGameEvent(token: $token)
     }
 `;
 
 type Props = { gameToken: string };
 
 const Game: NextPage<Props> = ({ gameToken }) => {
-    const router = useRouter();
-    const { code } = router.query;
-    const { data, loading } = useSubscription(GAME_SUBSCRIPTION, { variables: { code, token: getCookie('game_token') || gameToken } });
-
-    console.log(loading, data);
+    const { data } = useSubscription(GAME_SUBSCRIPTION, { variables: { token: getCookie('game_token') || gameToken } });
+    console.log(data);
 
     return <div></div>;
 };
