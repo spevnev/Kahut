e0d685ece2e65e4597776b4e54e076666fb08e1d
Toxyk <toxyflog.games@gmail.com>
2022-10-29 19:40:45 +0300

    Moving jobScheduler; Adding option to scheduleAt

---
+++ src/db/jobScheduler/publisher.ts
@@ -0,0 +1,56 @@
+import { ClientConfig } from 'pg';
+import { v4 as generateUUID } from 'uuid';
+import GenericClient from './client';
+
+export type PublisherConfig = {
+    batch_size?: number;
+    batch_threshold?: number;
+};
+
+type Entry = {
+    uuid: string;
+    data: any;
+    scheduleAt?: Date;
+};
+
+const DEFAULT_CONFIG: PublisherConfig = {
+    batch_size: 100,
+    batch_threshold: 333,
+};
+
+class Publisher extends GenericClient<PublisherConfig> {
+    private batch: Entry[] = [];
+    private batchTimeout: NodeJS.Timeout | null = null;
+
+    constructor(table: string, connectionConfig: ClientConfig, schedulerConfig: PublisherConfig = {}, schema = 'public') {
+        super(table, schema, connectionConfig, { ...DEFAULT_CONFIG, ...schedulerConfig });
+    }
+
+    private async _pub() {
+        const values = this.batch
+            .map(({ data, uuid, scheduleAt }) => `('${uuid}', ${this.client.escapeLiteral(JSON.stringify(data))}, ${scheduleAt || '-infinity'})`)
+            .join(', ');
+
+        this.batch = [];
+        this.batchTimeout = null;
+
+        await this.client.query(`INSERT INTO ${this.table} (job_id, data, taken_until) VALUES ${values};`, []);
+    }
+
+    async pub(data: any, scheduleAt?: Date): Promise<string> {
+        if (!this.client) await this.init();
+
+        const uuid = generateUUID();
+        this.batch.push({ uuid, data, scheduleAt });
+
+        if (this.batch.length === this.schedulerConfig.batch_size) {
+            if (this.batchTimeout) clearTimeout(this.batchTimeout);
+            await this._pub();
+        }
+        if (!this.batchTimeout) this.batchTimeout = setTimeout(this._pub.bind(this), this.schedulerConfig.batch_threshold);
+
+        return uuid;
+    }
+}
+
+export default Publisher;
