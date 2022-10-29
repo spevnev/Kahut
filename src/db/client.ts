a4509c21f4f3707797f61fb5842046f16c6ea76a
Toxyk <toxyflog.games@gmail.com>
2022-10-29 12:49:53 +0300

    Small changes

--- src/db/client.ts
+++ src/db/client.ts
@@ -1,12 +1,12 @@
-import { Client } from 'pg';
+import { Pool } from 'pg';
 import initDB from './initDB';
 
-export type DBClient = Client;
+export type DBClient = Pool;
 
 let client: DBClient | undefined;
 const initClient = async () => {
     if (client) return client;
-    client = new Client({ connectionString: process.env.DB_CONNECTION_STRING });
+    client = new Pool({ connectionString: process.env.DB_CONNECTION_STRING, max: 5 });
 
     await client.connect();
     await initDB(client);
