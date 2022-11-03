9479413ee9b3d7da1b6068ed0afb0299dcf2ae10
Toxyk <toxyflog.games@gmail.com>
2022-11-03 22:12:41 +0200

    Adding PubSub

--- src/graphql/gamePubSub.ts
+++ src/graphql/gamePubSub.ts
@@ -2,5 +2,12 @@ import { PubSub } from 'graphql-subscriptions';
 
 const pubsub = new PubSub();
 
-export const publish = (lobbyId: string, payload: any) => pubsub.publish(`GAME_EVENT_${lobbyId}`, { onGameEvent: JSON.stringify(payload) });
+type GameEventData = {
+    event: string;
+    data: { [key: string]: any };
+};
+export const publish = (lobbyId: string, { event, data }: GameEventData) => {
+    pubsub.publish(`GAME_EVENT_${lobbyId}`, { onGameEvent: { event, data: JSON.stringify(data) } });
+};
+
 export default pubsub;
