f3d58b2d5116d1d8f1a64324ecbb9ef71b4a36c0
Toxyk <toxyflog.games@gmail.com>
2022-11-12 17:40:41 +0200

    Sending questions with answers

--- src/jobs/showAnswer.ts
+++ src/jobs/showAnswer.ts
@@ -20,17 +20,18 @@ const GET_SCORE = `SELECT username, score FROM players WHERE lobby_id = $1;`;
 const showAnswer = async ({ lobbyId, questions, finishTime }: { lobbyId: string; finishTime: Date; questions: Question[] }) => {
     finishTime = new Date(finishTime);
 
-    const client = await getClient();
-    const { answers: correctAnswers, id: questionId, time, index } = questions.shift()!;
+    const question = questions.shift();
+    if (!question) throw new Error();
 
-    const getAnswersRes = await client.query(GET_ANSWERS, [lobbyId, questionId]);
+    const client = await getClient();
+    const getAnswersRes = await client.query(GET_ANSWERS, [lobbyId, question.id]);
 
     const addScore: { username: string; addScore: number }[] = [];
     getAnswersRes.rows.forEach(({ username, answers, answered_at }: { username: string; answers: number[]; answered_at: Date }) => {
-        if (answered_at > finishTime || correctAnswers.length !== answers.length) return;
-        for (const answer of answers) if (!correctAnswers.includes(Number(answer))) return;
+        if (answered_at > finishTime || question.answers.length !== answers.length) return;
+        for (const answer of answers) if (!question.answers.includes(Number(answer))) return;
 
-        let timeLeftPercentage = (finishTime.getTime() - answered_at.getTime()) / (time * 1000);
+        let timeLeftPercentage = (finishTime.getTime() - answered_at.getTime()) / (question.time * 1000);
         if (timeLeftPercentage > 0.9) timeLeftPercentage = 1;
         if (timeLeftPercentage < 0.2) timeLeftPercentage = 0;
 
@@ -41,10 +42,10 @@ const showAnswer = async ({ lobbyId, questions, finishTime }: { lobbyId: string;
     if (values.length > 0) await client.query(UPDATE_SCORE(values.join(', ')), [lobbyId]);
 
     const getScoreRes = await client.query(GET_SCORE, [lobbyId]);
-    publish(lobbyId, { event: 'SHOW_ANSWER', data: { answers: correctAnswers, points: getScoreRes.rows } });
+    publish(lobbyId, { event: 'SHOW_ANSWER', data: { question, points: getScoreRes.rows } });
 
     const nextEventDate = new Date(Date.now() + 10 * 1000);
-    if (questions.length === 0) await getPublishers().endGamePub.pub({ lobbyId, questionsNum: index + 1 }, nextEventDate);
+    if (questions.length === 0) await getPublishers().endGamePub.pub({ lobbyId, questionsNum: question.index + 1 }, nextEventDate);
     else await getPublishers().showQuestionPub.pub({ lobbyId, questions }, nextEventDate);
 };
 
