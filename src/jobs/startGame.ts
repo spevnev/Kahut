fc7aceff75cf8de1f81f833aaa31d0ceaa040595
Toxyk <toxyflog.games@gmail.com>
2022-11-20 19:28:18 +0200

    using one query in startGame

--- src/jobs/startGame.ts
+++ src/jobs/startGame.ts
@@ -2,34 +2,30 @@ import getClient from '../db/client';
 import { getPublishers } from '../db/jobScheduler/schedulers';
 import { publish } from '../graphql/gamePubSub';
 
-const GET_GAME = `
-    SELECT g.* FROM games g 
-    INNER JOIN lobbies l ON g.id = l.game_id 
-    WHERE l.code = $1;
-`;
-
-const GET_QUESTIONS = `
-    SELECT id, title, image, type, time, choices, answers 
-    FROM questions 
-    WHERE game_id = $1;
-`;
-
-const CLOSE_LOBBY = `
-    UPDATE lobbies 
-    SET state = 'INGAME' 
-    WHERE code = $1 AND state = 'OPEN';
+const CLOSE_LOBBY_AND_GET_GAME = `
+    WITH close_lobby AS (
+        UPDATE lobbies
+        SET state = 'INGAME'
+        WHERE code = $1 AND state = 'OPEN';
+    )
+    SELECT title, image, game_questions
+    FROM (
+        SELECT
+            game_id,
+            JSONB_AGG(TO_JSONB(questions.*)) AS game_questions
+        FROM questions
+        WHERE game_id = (SELECT game_id FROM lobbies WHERE code = $1)
+        GROUP BY game_id
+    ) AS game_questions
+    INNER JOIN games ON game_id = id;
 `;
 
 const startGame = async ({ lobbyId }: { lobbyId: string }) => {
     const client = await getClient();
 
-    const getGameRes = await client.query(GET_GAME, [lobbyId]);
-    const { id, title, image } = getGameRes.rows[0];
-
-    const getQuestionsRes = await client.query(GET_QUESTIONS, [id]);
-    const questions = getQuestionsRes.rows.map((question, index) => ({ ...question, index }));
-
-    await client.query(CLOSE_LOBBY, [lobbyId]);
+    const res = await client.query(CLOSE_LOBBY_AND_GET_GAME, [lobbyId]);
+    const { title, image, game_questions } = res.rows[0];
+    const questions = (game_questions as any[]).map((question, index) => ({ ...question, index }));
 
     publish(lobbyId, { event: 'START_GAME', data: { title, image } });
 
